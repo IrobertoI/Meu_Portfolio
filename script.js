40b1b3bd5d7a4cfde927feae522ccf04444497b7
Cláudio Roberto <claudiorobertodemenezesjunior@gmail.com>
2024-10-13 12:56:54 -0300

    pagina inicial e sobre

---
+++ script.js
@@ -0,0 +1,30 @@
+function ativaLetra2(elemento){
+    const arrTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrTexto.forEach((Letra, i)=>{
+        setTimeout(()=>{
+            elemento.innerHTML += Letra;
+        }, 80 * i);
+    });
+}
+
+const texto = document.querySelector('.digitar');
+ativaLetra(texto);
+
+
+function ativaLetra(elemento){
+    const arrTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrTexto.forEach((Letra, i)=>{
+        setTimeout(()=>{
+            elemento.innerHTML += Letra;
+        }, 80 * i);
+    });
+}
+
+const titulo = document.querySelector('.digitando');
+ativaLetra(titulo);
+
+
+
+
