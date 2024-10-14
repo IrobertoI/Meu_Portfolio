62ce792f5bba6bd627b8e704fb8fe387d60c0cd8
Cláudio Roberto <claudiorobertodemenezesjunior@gmail.com>
2024-10-13 22:41:08 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-function ativaLetra2(elemento){
-    const arrTexto = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    arrTexto.forEach((Letra, i)=>{
-        setTimeout(()=>{
-            elemento.innerHTML += Letra;
-        }, 80 * i);
-    });
+function escrevendoLetra(){
+
+    function ativaLetra2(elemento){
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((Letra, i)=>{
+            setTimeout(()=>{
+                elemento.innerHTML += Letra;
+            }, 80 * i);
+        });
+    }
+    
+    const texto = document.querySelector('.digitar');
+    ativaLetra(texto);
+
+
+    function ativaLetra(elemento){
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((Letra, i)=>{
+            setTimeout(()=>{
+                elemento.innerHTML += Letra;
+            }, 80 * i);
+        });
+    }
+    
+    const titulo = document.querySelector('.digitando');
+    ativaLetra(titulo);
+
 }
 
-const texto = document.querySelector('.digitar');
-ativaLetra(texto);
+
+const ativaMenu = document.querySelector('fa-bars');
+
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x')
+})
 
 
-function ativaLetra(elemento){
-    const arrTexto = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    arrTexto.forEach((Letra, i)=>{
-        setTimeout(()=>{
-            elemento.innerHTML += Letra;
-        }, 80 * i);
-    });
-}
 
-const titulo = document.querySelector('.digitando');
-ativaLetra(titulo);
+
 
 
 
